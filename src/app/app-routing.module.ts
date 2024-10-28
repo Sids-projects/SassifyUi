5c5e594d06173665024940af8e31d3fd6b885a78
Siddarthg-Dev <siddarthg.dev@gmail.com>
2024-10-28 13:37:49 +0530

    Created base in app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComponentsComponent } from './screens/components/components.component';
+import { TemplatesComponent } from './screens/templates/templates.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Component', component: ComponentsComponent },
+  { path: 'Templates', component: TemplatesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+// Modules
+import { MatIconModule } from '@angular/material/icon';
+import { ComponentsComponent } from './screens/components/components.component';
+import { TemplatesComponent } from './screens/templates/templates.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, ComponentsComponent, TemplatesComponent],
+  imports: [BrowserModule, AppRoutingModule, MatIconModule],
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
