23b5ba04964e89d44acf57d2d6b2acd9be5930e2
Siddarthg-Dev <siddarthg.dev@gmail.com>
2024-11-25 21:53:38 +0530

    Table App layout fix with build

--- docs/chunk-IA34MTUR.js
+++
@@ -1,1 +0,0 @@
-import{h as a}from"./chunk-ORBZPBFN.js";import"./chunk-HOTVCXV5.js";import"./chunk-NK5KMFGN.js";export{a as UiCompsModule};
---
+++ docs/chunk-RCZYMHIF.js
@@ -0,0 +1,1 @@
+import{h as a}from"./chunk-6EZU4CUO.js";import"./chunk-HOTVCXV5.js";import"./chunk-NK5KMFGN.js";export{a as UiCompsModule};
